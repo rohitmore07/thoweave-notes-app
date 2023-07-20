2c271bcc0bb838b7abb6550638abf0a8126a3aba
Rohit More <cserohitmore@gmail.com>
2023-07-20 17:37:10 +0530

    Initial commit

---
+++ src/components/Content.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Content() {
+  return (
+    <>
+    <div className="main-content">
+    <h1>Welcome to Thoweave!</h1>
+    <p>Your one-stop destination for seamless note-taking, organization, and productivity!<br/> 
+        Whether you need to jot down quick thoughts, create to-do lists,<br/>or save important 
+        information, Thoweave has got you covered.</p>
+    </div>
+    </>
+  );
+}
+
+export default Content;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <p>Made By Rohit.</p>
+    </footer>
+  );
+}
+
+export default Footer;
